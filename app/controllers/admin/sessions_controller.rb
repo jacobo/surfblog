7d783517b7de7433403245cb44dac3eb1ae8edd4
Jacob Burkhart <jburkhart@engineyard.com>
2012-02-03 14:12:36 -0800

    messing with oauth

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -10,6 +10,31 @@ class Admin::SessionsController < ApplicationController
   end
 
   def new
+    render :text => params.inspect
+#     # require 'oauth2'
+#     client_id = "f3c1db38c7d34934f239"
+#     client_secret = "8330407e03de5dfff4023ae1eeacd59e745afcb6"
+#
+# https://github.com/login/oauth/authorize?response_type=code&client_id=f3c1db38c7d34934f239&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fcallback
+#
+#
+# POST https://github.com/login/oauth/access_token?
+#   client_id=...&
+#   redirect_uri=http://www.example.com/oauth_redirect&
+#   client_secret=...&
+#   code=...
+#
+#     client = OAuth2::Client.new(client_id, client_secret, :site => 'https://github.com', :authorize_url => '/login/oauth/authorize')
+#     client.auth_code.authorize_url(:redirect_uri => 'http://localhost:3000/auth/callback')
+#
+#
+#     token = client.auth_code.get_token('a80c560daf127586b73b', :redirect_uri => 'http://localhost:3000/auth/callback')
+#     # response = token.get('/api/resource', :params => { 'query_foo' => 'bar' })
+#
+#     GET https://github.com/api/v2/json/user/show?
+#       access_token=...
+
+
   end
 
   def create
