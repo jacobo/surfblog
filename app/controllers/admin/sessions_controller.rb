a5f548356f24348fba2685b4d3a4b6fc141ca72a
Jacob Burkhart & Larry Diehl <jburkhart@engineyard.com>
2012-02-03 17:42:02 -0800

    WIP

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -11,9 +11,9 @@ class Admin::SessionsController < ApplicationController
 
   def new
     render :text => params.inspect
-#     # require 'oauth2'
-#     client_id = "f3c1db38c7d34934f239"
-#     client_secret = "8330407e03de5dfff4023ae1eeacd59e745afcb6"
+    # require 'oauth2'
+    client_id = "f3c1db38c7d34934f239"
+    client_secret = "8330407e03de5dfff4023ae1eeacd59e745afcb6"
 #
 # https://github.com/login/oauth/authorize?response_type=code&client_id=f3c1db38c7d34934f239&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fcallback
 #
@@ -24,16 +24,28 @@ class Admin::SessionsController < ApplicationController
 #   client_secret=...&
 #   code=...
 #
-#     client = OAuth2::Client.new(client_id, client_secret, :site => 'https://github.com', :authorize_url => '/login/oauth/authorize')
-#     client.auth_code.authorize_url(:redirect_uri => 'http://localhost:3000/auth/callback')
+    require 'oauth2'
+    client = OAuth2::Client.new(client_id, client_secret, :site => 'https://github.com', :authorize_url => '/login/oauth/authorize', :token_url => "/login/oauth/access_token")
+    client.auth_code.authorize_url(:redirect_uri => 'http://localhost:3000/auth/callback')
 #
 #
-#     token = client.auth_code.get_token('a80c560daf127586b73b', :redirect_uri => 'http://localhost:3000/auth/callback')
-#     # response = token.get('/api/resource', :params => { 'query_foo' => 'bar' })
-#
-#     GET https://github.com/api/v2/json/user/show?
-#       access_token=...
+    token = client.auth_code.get_token('26e8e05be8fc29761f7c', :redirect_uri => 'http://localhost:3000/auth/callback')
 
+    # response = token.get('/api/resource', :params => { 'query_foo' => 'bar' })
+
+    curl -v -X POST "https://github.com/login/oauth/access_token?client_id=f3c1db38c7d34934f239&redirect_uri=http://localhost:3000/auth/callback&client_secret=8330407e03de5dfff4023ae1eeacd59e745afcb6&code=221a75b8b043befb3aef"
+
+    token.token
+
+    token.get("/api/v2/json/user/show?access_token=#{token.token}")
+    #or any other way to GET to this URL
+
+  curl -v -X GET "https://github.com/api/v2/json/user/show?access_token=e2898a055ccad7356eda98cba1749706141d5a1c"
+
+#
+#     GET https://github.com/api/v2/json/user/show?access_token=...
+
+"https://github.com/api/v2/json/user/show?access_token=e2898a055ccad7356eda98cba1749706141d5a1c"
 
   end
 
